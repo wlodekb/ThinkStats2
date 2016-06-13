329418bd7ee431d03b64f21b9a2a61132163473a
Wlodzimierz Borkowski <wlodzimierz.borkowski@intel.com>
2016-06-13 21:44:30 +0200

    validate two data frame in ex01

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -7,11 +7,30 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
 from __future__ import print_function
 
-import numpy as np
 import sys
-
 import nsfg
 import thinkstats2
+
+
+def ValidatePregnum(resp):
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(preg)
+    for i, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[i]
+        if len(preg_map[caseid]) != pregnum:
+            return False
+    return True
+
+
+def CleanFemResp(df):
+    pass
+
+
+def ReadFemResp(dct_file="2002FemResp.dct", dat_file="2002FemResp.dat.gz"):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    CleanFemResp(df)
+    return df
 
 
 def main(script):
@@ -20,12 +39,11 @@ def main(script):
     script: string script name
     http://www.icpsr.umich.edu/nsfg6/Controller?displayPage=labelDetails&fileCode=FEM&section=R&subSec=7869&srtLabel=606835
     """
-    dct = thinkstats2.ReadStataDct("2002FemResp.dct")
-    df = dct.ReadFixedWidth("2002FemResp.dat.gz", compression='gzip')
-    pregnum = df.pregnum.value_counts().sort_index()
-    preg_map = nsfg.MakePregMap(df)
-    print(preg_map)
-    print(pregnum)
+    resp = ReadFemResp()
+    assert len(resp) == 7643
+    assert (resp.pregnum.value_counts()[1] == 1267)
+
+    assert ValidatePregnum(resp)
 
 
 if __name__ == '__main__':
