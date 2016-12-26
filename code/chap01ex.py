a16804f46b3c8def6aac82874212189d6eb6a0ae
Wlodzimierz Borkowski <wlodzimierz.borkowski@intel.com>
2016-12-26 17:43:01 +0100

    fix chap01 exc

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -8,42 +8,52 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 from __future__ import print_function
 
 import sys
+
 import nsfg
 import thinkstats2
 
 
-def ValidatePregnum(resp):
-    preg = nsfg.ReadFemPreg()
-    preg_map = nsfg.MakePregMap(preg)
-    for i, pregnum in resp.pregnum.iteritems():
-        caseid = resp.caseid[i]
-        if len(preg_map[caseid]) != pregnum:
-            return False
-    return True
+def preg_times(preg_map):
+    return {k: len(v) for k, v in preg_map.items()}
 
 
-def CleanFemResp(df):
-    pass
-
-
-def ReadFemResp(dct_file="2002FemResp.dct", dat_file="2002FemResp.dat.gz"):
+def ReadFemResp(dct_file='2002FemPreg.dct',
+                dat_file='2002FemPreg.dat.gz'):
     dct = thinkstats2.ReadStataDct(dct_file)
     df = dct.ReadFixedWidth(dat_file, compression='gzip')
-    CleanFemResp(df)
     return df
+
+
+def validate(resp):
+    assert len(resp.index) == 13593
+    assert resp.pregnum.value_counts(1) == 1267
+
+    preg_map = nsfg.MakePregMap(resp)
+    preg_times_map = preg_times(preg_map)
+
+    for caseid, indicies in preg_map.items():
+        pregnum_resp = resp.loc[indicies, 'pregnum']
+        assert len(pregnum_resp) == preg_times_map[caseid]
+
+        caseid_resp = resp.caseid[indicies]
+        assert caseid_resp.tolist() == preg_map[caseid]
 
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
-    http://www.icpsr.umich.edu/nsfg6/Controller?displayPage=labelDetails&fileCode=FEM&section=R&subSec=7869&srtLabel=606835
     """
-    resp = ReadFemResp()
-    assert len(resp) == 7643
-    assert (resp.pregnum.value_counts()[1] == 1267)
+    df = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(df)
+    preg_times_map = preg_times(preg_map)
 
-    assert ValidatePregnum(resp)
+    for k, v in preg_times_map.items():
+        print("subject %s was pregnant %s times" % (k, v))
+
+    resp = ReadFemResp()
+    validate(resp)
+    print('%s: All tests passed.' % script)
 
 
 if __name__ == '__main__':
